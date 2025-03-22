85fe347ad2959745b7e4fd97c412ff44287836d5
HashirBaig <hashirbaig456@gmail.com>
2025-03-22 17:33:43 +0100

    added color map

--- js/controllers/product.js
+++ js/controllers/product.js
@@ -12,25 +12,29 @@ const initMap = () => {
 
   // var url_to_geotiff_file = `https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/ImagebasedDSM.tif?alt=media&token=9eda9d88-4578-48a1-ba78-1effe9e51aa6`;
   var url_to_geotiff_file =
-    "https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/1106.tif?alt=media&token=5b19ec74-26c0-4b1d-a240-2bde75019829";
+    "https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/exported_enschede_dsm.tif?alt=media&token=1aa5910e-ef4f-48ba-afb7-4031fff16121";
+  // var url_to_geotiff_file =
+  //   "https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/1106.tif?alt=media&token=5b19ec74-26c0-4b1d-a240-2bde75019829";
 
   fetch(url_to_geotiff_file)
     .then((res) => res.arrayBuffer())
     .then((arrayBuffer) => {
       parseGeoraster(arrayBuffer).then((georaster) => {
         const numberOfItems = Math.round(georaster.maxs[0]);
+
+        // Create a Rainbow color scale from blue (low) to red (high)
         const rainbow = new Rainbow();
         rainbow.setNumberRange(1, numberOfItems);
-        rainbow.setSpectrum("white", "red");
+        rainbow.setSpectrum("blue", "cyan", "green", "yellow", "red"); // Gradient from blue to red
 
         const layer = new GeoRasterLayer({
           georaster: georaster,
           opacity: 0.7,
-          pixelValuesToColorFn: (vals) =>
-            Math.round(vals[0]) <= 0
-              ? null
-              : "#" + rainbow.colourAt(Math.round(vals[0])),
-          resolution: 512, // optional parameter for adjusting display resolution
+          pixelValuesToColorFn: (vals) => {
+            const value = Math.round(vals[0]);
+            return value <= 0 ? null : "#" + rainbow.colourAt(value); // Map values to colors
+          },
+          resolution: 512, // Adjust display resolution
         }).addTo(map);
 
         map.fitBounds(layer.getBounds());
