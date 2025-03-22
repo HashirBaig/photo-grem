45304548b6fd6adfdcbf01fc1c32f115522ad89c
HashirBaig <hashirbaig456@gmail.com>
2025-03-22 15:53:53 +0100

    added firebase and displayed sample tif

--- js/controllers/product.js
+++ js/controllers/product.js
@@ -3,42 +3,37 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const initMap = () => {
-  var map = L.map("map").setView([52.240304, 6.853039], 16); // Centered on Amsterdam
-
-  // Add a basemap
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap contributors",
+  var map = L.map("map").setView([52.240304, 6.853039], 16);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  // Load GeoTIFF
-  var geoTiffUrl = `assets/data/geotiff_dsm.tif`; // Replace with your TIFF file path
+  // var url_to_geotiff_file = `https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/ImagebasedDSM.tif?alt=media&token=9eda9d88-4578-48a1-ba78-1effe9e51aa6`;
+  var url_to_geotiff_file =
+    "https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/1106.tif?alt=media&token=5b19ec74-26c0-4b1d-a240-2bde75019829";
 
-  parseGeoraster(geoTiffUrl).then((georaster) => {
-    console.log("geotiff: ", georaster.height);
-  });
+  fetch(url_to_geotiff_file)
+    .then((res) => res.arrayBuffer())
+    .then((arrayBuffer) => {
+      parseGeoraster(arrayBuffer).then((georaster) => {
+        const numberOfItems = Math.round(georaster.maxs[0]);
+        const rainbow = new Rainbow();
+        rainbow.setNumberRange(1, numberOfItems);
+        rainbow.setSpectrum("white", "red");
 
-  // var geotiffLayer = L.leafletGeotiff(geoTiffUrl, {
-  //   band: 0,
-  //   renderer: L.LeafletGeotiff.plotty({
-  //     displayMin: 0,
-  //     displayMax: 255,
-  //     colorScale: "viridis",
-  //   }),
-  // }).addTo(map);
+        const layer = new GeoRasterLayer({
+          georaster: georaster,
+          opacity: 0.7,
+          pixelValuesToColorFn: (vals) =>
+            Math.round(vals[0]) <= 0
+              ? null
+              : "#" + rainbow.colourAt(Math.round(vals[0])),
+          resolution: 512, // optional parameter for adjusting display resolution
+        }).addTo(map);
 
-  // fetch(geoTiffUrl)
-  //   .then((response) => response.arrayBuffer())
-  //   .then((arrayBuffer) => {
-  //     parseGeoraster(arrayBuffer).then((georaster) => {
-  //       const layer = new GeoRasterLayer({
-  //         georaster: georaster,
-  //         opacity: 0.7,
-  //         pixelValuesToColorFn: (values) =>
-  //           values[0] === 42 ? "#ffffff" : "#000000",
-  //         resolution: 64, // Optional parameter for adjusting display resolution
-  //       });
-  //       layer.addTo(map);
-  //       map.fitBounds(layer.getBounds());
-  //     });
-  //   });
+        map.fitBounds(layer.getBounds());
+      });
+    });
 };
