0b8ef8f80623440a7237a813c7e05b0bc1241401
HashirBaig <hashirbaig456@gmail.com>
2025-03-23 14:45:48 +0100

    added control panel for raster

--- js/controllers/product.js
+++ js/controllers/product.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   initMap();
 });
 
+// Global Variable
+let geoRasterLayer = null;
+
 const initMap = () => {
   var map = L.map("map").setView([52.240304, 6.853039], 16);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -10,6 +13,76 @@ const initMap = () => {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
+  // Add Geo Raster
+  renderGeoRaster(map);
+
+  // Add Control Panel
+  renderControlPanel(map);
+};
+
+const renderControlPanel = (map) => {
+  let panelControl = L.Control.extend({
+    options: {
+      position: "topright", // Change to 'topleft', 'bottomleft', or 'bottomright'
+    },
+
+    onAdd: function () {
+      const panelControlDiv = L.DomUtil.create(
+        "div",
+        "leaflet-bar leaflet-control leaflet-control-custom bg-white"
+      );
+
+      panelControlDiv.style.width = "12rem";
+      panelControlDiv.style.padding = "12px";
+      panelControlDiv.style.borderRadius = "6px";
+      panelControlDiv.style.backgroundColor = "#fdedec";
+
+      // Header
+      const headerDiv = document.createElement("h5");
+      headerDiv.className = "text-center fw-bolder";
+      headerDiv.innerText = "Controls";
+
+      // Checkbox and Label
+      const checkboxContainer = document.createElement("div");
+      checkboxContainer.className = "form-check fs-custom";
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = "toggleDSMRaster";
+      checkbox.className = "form-check-input cursor-pointer";
+      checkbox.checked = true;
+
+      const label = document.createElement("label");
+      label.htmlFor = "toggleDSMRaster";
+      label.innerText = "Show DSM";
+      label.className = "form-check-label";
+
+      // Append elements
+      checkboxContainer.appendChild(checkbox);
+      checkboxContainer.appendChild(label);
+
+      panelControlDiv.appendChild(headerDiv);
+      panelControlDiv.appendChild(checkboxContainer);
+
+      // On change methods
+      checkbox.onchange = function () {
+        if (this.checked) {
+          geoRasterLayer.addTo(map);
+        } else {
+          map.removeLayer(geoRasterLayer);
+        }
+      };
+
+      // Return the panel div
+      return panelControlDiv;
+    },
+  });
+
+  // Add Control Panel to Map
+  map.addControl(new panelControl());
+};
+
+const renderGeoRaster = (map) => {
   var url_to_geotiff_file =
     "https://firebasestorage.googleapis.com/v0/b/mpn-dev-67647.appspot.com/o/exported_enschede_dsm.tif?alt=media&token=1aa5910e-ef4f-48ba-afb7-4031fff16121";
 
@@ -24,7 +97,7 @@ const initMap = () => {
         rainbow.setNumberRange(1, numberOfItems);
         rainbow.setSpectrum("blue", "cyan", "green", "yellow", "red"); // Gradient from blue to red
 
-        const layer = new GeoRasterLayer({
+        geoRasterLayer = new GeoRasterLayer({
           georaster: georaster,
           opacity: 0.7,
           pixelValuesToColorFn: (vals) => {
@@ -34,7 +107,7 @@ const initMap = () => {
           resolution: 512, // Adjust display resolution
         }).addTo(map);
 
-        map.fitBounds(layer.getBounds());
+        map.fitBounds(geoRasterLayer.getBounds());
       });
     });
 };
